13229d22add38b75fdbb2acf1a60b9aa56a36ade
Andrea <andrelorenzon@gmail.com>
2019-12-04 10:41:55 +0100

    added readings for digit 2 and 6

--- src/main/java/bankocr/kata/Cell.java
+++ src/main/java/bankocr/kata/Cell.java
@@ -8,17 +8,9 @@ public class Cell {
                     "|_|";
 
     private static final String ONE_CELL =
-                    "  |" +
+                    "   " +
                     "  |" +
                     "  |";
-
-    private static final String FIVE_CELL =
-                    " _ " +
-                    "|_ " +
-                    " _|";
-
-    private final String[] numbers = {ZERO_CELL, THREE_CELL, FIVE_CELL};
-
 
     private static final String TWO_CELL =
                     " _ "+
@@ -31,10 +23,13 @@ public class Cell {
                     " _|";
 
     private static final String FOUR_CELL =
-            "   " +
+                    "   " +
                     "|_|" +
                     "  |";
-
+    private static final String FIVE_CELL =
+                    " _ " +
+                    "|_ " +
+                    " _|";
     private static final String SIX_CELL =
                     " _ "+
                     "|_ "+
@@ -45,15 +40,30 @@ public class Cell {
                     "  |" +
                     "  |";
 
-
     private static final String EIGHT_CELL =
-		    " _ " +
+		            " _ " +
                     "|_|" +
                     "|_|";
 
+    private static final String NINE_CELL =
+                    " _ " +
+                    "|_|" +
+                    " _|";
+
+    private final String[] numbers = {ZERO_CELL,
+            ONE_CELL,
+            TWO_CELL,
+            THREE_CELL,
+            FOUR_CELL,
+            FIVE_CELL,
+            SIX_CELL,
+            SEVEN_CELL,
+            EIGHT_CELL,
+            NINE_CELL};
 
 
     private String cellAsText;
+
 
     public Cell(String cellAsText) {
         this.cellAsText = cellAsText;
